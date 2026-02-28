0e62103cacbd9f8afb0b453e27b74598df2b8ee5
Carlos Barra <carloseduardobc27@gmail.com>
2026-02-28 15:54:36 -0500

    Avance hasta paso 4

---
+++ src/main/java/com/aluracursos/literalura/model/DatosAutor.java
@@ -0,0 +1,12 @@
+package com.aluracursos.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+// Ignora cualquier campo del JSON que no hayamos declarado aquí
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+        @JsonAlias("name") String nombre,
+        @JsonAlias("birth_year") String fechaDeNacimiento
+) {
+}
